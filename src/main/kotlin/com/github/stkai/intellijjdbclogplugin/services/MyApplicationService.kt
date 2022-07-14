23e18c29209950b92deb1d84359cec68a1d31dde
GitHub Action <action@github.com>
2022-07-14 14:01:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/stkai/intellijjdbclogplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.stkai.intellijjdbclogplugin.services
+
+import com.github.stkai.intellijjdbclogplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/test/kotlin/com/github/stkai/intellijjdbclogplugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/stkai/intellijjdbclogplugin/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.stkai.intellijjdbclogplugin
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
